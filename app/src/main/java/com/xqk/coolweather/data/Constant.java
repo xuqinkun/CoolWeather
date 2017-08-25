31cf524bf030ee106cf3179b9a4a5887a4416e42
xuqinkun <15528498250@163.com>
2017-08-25 10:35:27 +0800

    增加后台自动更新天气的功能

---
+++ app/src/main/java/com/xqk/coolweather/data/Constant.java
@@ -0,0 +1,10 @@
+package com.xqk.coolweather.data;
+
+/**
+ * Created by xuqin on 2017/8/25.
+ */
+
+public class Constant {
+    public static final String myKey = "e899d1e256234c8ebeb51850f7ea934f";
+    public static final String REQUEST_WEATHER_URL = "http://guolin.tech/api/weather?cityid=";
+}
